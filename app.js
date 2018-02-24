8364b053542d24f159f8d86700cebe886f4651ac
Callum Beckwith <123callumb@gmail.com>
2018-02-24 16:52:56 +0000

    Big start
    Okay this contains all the main frameworks on the server we will use.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const serv = require('http').Server(app);
+const sokcet = require('socket.io')(serv, {});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/test.html');
+});
+
+serv.listen(1337);
